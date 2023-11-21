40a1727cb502e0013dd0bc34b87e36aad38bab6c
yimeca <yitzchokmeircarr@gmail.com>
2023-11-21 15:07:33 +0000

    Added a class for DateOption

--- change_time.py
+++ change_time.py
@@ -1,11 +1,31 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title("Change The Time and Date")
+# Classes
 
-mainframe = ttk.Frame(root, padding="3 3 12 12").grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+class DateOption(ttk.Frame):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        button = ttk.Button(self, text="Press me", command=self.calculate)
+        button.grid(column=0, row=0, sticky=(N, W, E, S))
 
-root.mainloop()
+    def calculate(self):
+        pass
+
+# Main
+
+def main():
+
+    root = Tk()
+    root.title("Change The Time and Date")
+
+    mainframe = ttk.Frame(root, padding="3 3 12 12").grid(column=0, row=0, sticky=(N, W, E, S))
+    root.columnconfigure(0, weight=1)
+    root.rowconfigure(0, weight=1)
+
+    time1 = DateOption(root, padding="3 3 12 12").grid(column=0, row=0, sticky=(N, W, E, S))
+
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
