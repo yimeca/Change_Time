53007b58484a85d531c260790574abb4b5b83cfd
yimeca <yitzchokmeircarr@gmail.com>
2023-11-24 14:14:10 +0000

    Cleaned up Gui (Deleted unused code)

--- change_time.py
+++ change_time.py
@@ -6,75 +6,6 @@ from tktimepicker import AnalogPicker, SpinTimePickerModern, SpinTimePickerOld, 
 
 # Classes
 
-class DateOption(ttk.Frame):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        global root
-
-        # Day
-        increase_day = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=0, row=0)
-        self.validate_day_wrapper = (root.register(self.validate_day), "%P", "%V")
-        self.day_var = StringVar()
-        self.day_var.set("0")
-        day_entry = ttk.Entry(self, textvariable=self.day_var, validate="all",
-                               validatecommand=self.validate_day_wrapper).grid(column=0, row=1)
-        decrease_day = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=0, row=2)
-
-        # Month
-        increase_month = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=1, row=0)
-        self.validate_month_wrapper = (root.register(self.validate_month), "%P", "%V")
-        self.month_var = StringVar()
-        self.month_var.set("0")
-        month_entry = ttk.Entry(self, textvariable=self.month_var, validate="all",
-                               validatecommand=self.validate_month_wrapper).grid(column=1, row=1)
-        decrease_month = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=1, row=2)
-
-
-#        # Increase Buttons
-#        increase_day = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=0, row=0)
-#        increase_month = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=1, row=0)
-#        increase_year = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=2, row=0)
-#        
-#        increase_hour = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=3, row=0)
-#        increase_minute = ttk.Button(self, style="ChangeTime.TButton", text="↑").grid(column=4, row=0)
-#
-#        # Entry
-#        global root
-#        self.validate_wrapper = (root.register(self.validate), "%P", "%V")
-#        self.time_var = StringVar()
-#        self.time_var.set("00/00/0000 00:00")
-#        date_entry = ttk.Entry(self, textvariable=self.time_var, validate="all",
-#                               validatecommand=self.validate_wrapper).grid(column=0, row=1, columnspan=5)
-#        
-#        # Decrease Buttons
-#        decrease_day = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=0, row=2)
-#        decrease_month = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=1, row=2)
-#        decrease_year = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=2, row=2)
-#        
-#        increase_hour = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=3, row=2)
-#        increase_minute = ttk.Button(self, style="ChangeTime.TButton", text="↓").grid(column=4, row=2)
-#
-#
-#        button = ttk.Button(self, text="Press me", command=self.calculate)
-#        button.grid(column=0, row=6, sticky=(N, W, E, S))
-    
-    def validate_day(self, newval, op):
-        if op == "key":
-            print(newval)
-            if (re.match("^[0-9]*$", newval) is not None and  len(newval) <= 2 and
-                int(newval) <= 31):
-                return True
-        return True
-    
-    def validate_month(self, newval, op):
-        if op == "key":
-            print(newval)
-        return True
-
-    def calculate(self):
-        pass
-
 class TimeOption(ttk.LabelFrame):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -117,9 +48,6 @@ def main():
     global style
     style = ttk.Style()
     style.theme_use("clam")
-    style.configure("ChangeTime.TButton", width=2)
-    style.configure("Increase.ChangeTime.TButton", text="↑")
-    style.configure("Decrease.ChangeTime.TButton", text="↓")
 
     mainframe = ttk.Frame(root, padding="3 3 12 12").grid(column=0, row=0, sticky=(N, W, E, S))
     root.columnconfigure(0, weight=1)
@@ -130,20 +58,7 @@ def main():
     time3 = TimeOption(mainframe, padding="12 12 12 12").grid(row=1, column=0)
     time4 = TimeOption(mainframe, padding="12 12 12 12").grid(row=1, column=1)
 
-#    seperator = ttk.Separator(mainframe, orient=HORIZONTAL).grid(row=3, column=0, columnspan=3)
-#    reset_frame = ttk.LabelFrame(mainframe, padding="120 120 120 120").grid(row=3, column=0, columnspan=3, sticky=(N, W, E, S))
     reset = ttk.Button(mainframe, text="Reset Time and Date (Requires internet)").grid(row=3, column=0, columnspan=3)
-
-#    time1 = DateOption(root, padding="3 3 12 12").grid(column=0, row=0, sticky=(N, W, E, S))
-#    time2 = DateOption(root, padding="3 3 12 12").grid(column=0, row=1, sticky=(N, W, E, S))
-#    time3 = DateOption(root, padding="3 3 12 12").grid(column=0, row=2, sticky=(N, W, E, S))
-
-#    cal1 = Calendar(root, year=2020, month=5, day=22).grid(column=0, row=0)
-#    tim1 = AnalogPicker(root).grid(column=1, row=0)
-#    tim2 = SpinTimePickerModern(root, orient=constants.HORIZONTAL).grid(column=0, row=0)
-#    cal2 = Calendar(root, selectmode="day", year=2020, month=5, day=22).grid(column=0, row=1)
-#    cal3 = Calendar(root, selectmode="day", year=2020, month=5, day=22).grid(column=0, row=2)
-#    cal4 = Calendar(root, selectmode="day", year=2020, month=5, day=22).grid(column=0, row=3)
 
     root.mainloop()
 
