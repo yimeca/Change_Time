2c2533a346497f2393a19ef978996b9d73504f76
yimeca <yitzchokmeircarr@gmail.com>
2023-12-18 14:18:05 +0000

    Put internet access in a try statement

--- change_time.py
+++ change_time.py
@@ -71,27 +71,31 @@ class TimeOption(ttk.LabelFrame):
         print(self.hour_var.get(), self.min_var.get(), self.sec_var.get(), self.date_var.get())
 
     def reset_time_option(self):
-        timeapi = requests.get("http://worldtimeapi.org/api/timezone/Etc/GMT")
-        tuple_info = timeapi.json()['datetime']
+        try:
+            timeapi = requests.get("http://worldtimeapi.org/api/timezone/Etc/GMT")
+        except Exception:
+            messagebox.showwarning(message="You need internet for this")
+        else:
+            tuple_info = timeapi.json()['datetime']
 
-        year = int(tuple_info[0:4])
-        month = int(tuple_info[5:7])
-        day = int(tuple_info[8:10])
-        hour = tuple_info[11:13]
-        minute = tuple_info[14:16]
-        second= tuple_info[17:19]
+            year = int(tuple_info[0:4])
+            month = int(tuple_info[5:7])
+            day = int(tuple_info[8:10])
+            hour = tuple_info[11:13]
+            minute = tuple_info[14:16]
+            second= tuple_info[17:19]
 
-        date = f"{day}/{month}/{year}"
-        
-        self.date_var.set(date)
-        # Hour
-        self.hour_var.set(f"{hour:.2}")
-        # Minute
-        self.min_var.set(f"{minute:.2}")
-        # Second
-        self.sec_var.set(f"{second:.2}")
-        # Title
-        self.title_var.set("")
+            date = f"{day}/{month}/{year}"
+            
+            self.date_var.set(date)
+            # Hour
+            self.hour_var.set(f"{hour:.2}")
+            # Minute
+            self.min_var.set(f"{minute:.2}")
+            # Second
+            self.sec_var.set(f"{second:.2}")
+            # Title
+            self.title_var.set("")
     
     def __str__(self):
         
@@ -120,19 +124,23 @@ def win_change_time(time_tuple):
 
 # Reset Time Function
 def reset_time():
-    timeapi = requests.get("http://worldtimeapi.org/api/timezone/Etc/GMT")
-    tuple_info = timeapi.json()['datetime']
-    year = int(tuple_info[0:4])
-    month = int(tuple_info[5:7])
-    day = int(tuple_info[8:10])
-    hour = int(tuple_info[11:13])
-    minute = int(tuple_info[14:16])
-    second= int(tuple_info[17:19])
-    millisecond = 0
-    api_tuple = (year,month,day,hour,minute,second,millisecond)
-    win_change_time(api_tuple)
-    print("reset")
-    print(year,month,day,hour,minute,second,millisecond)
+    try:
+        timeapi = requests.get("http://worldtimeapi.org/api/timezone/Etc/GMT")
+    except Exception:
+        messagebox.showwarning(message="You need internet for this")
+    else:
+        tuple_info = timeapi.json()['datetime']
+        year = int(tuple_info[0:4])
+        month = int(tuple_info[5:7])
+        day = int(tuple_info[8:10])
+        hour = int(tuple_info[11:13])
+        minute = int(tuple_info[14:16])
+        second= int(tuple_info[17:19])
+        millisecond = 0
+        api_tuple = (year,month,day,hour,minute,second,millisecond)
+        win_change_time(api_tuple)
+        print("reset")
+        print(year,month,day,hour,minute,second,millisecond)
 
 # On close
 
@@ -183,7 +191,17 @@ def main():
     time4.grid(row=1, column=1)
 
     reset = ttk.Button(mainframe, text="Reset Time and Date (Requires internet)", command=reset_time
-                       ).grid(row=3, column=0, columnspan=3)
+                       ).grid(row=3, column=0, columnspan=1)
+    
+    list_of_time_zones = ['Africa/Abidjan', 'Africa/Algiers', 'Africa/Bissau', 'Africa/Cairo', 'Africa/Casablanca', 'Africa/Ceuta', 'Africa/El_Aaiun', 'Africa/Johannesburg', 'Africa/Juba', 'Africa/Khartoum', 'Africa/Lagos', 'Africa/Maputo', 'Africa/Monrovia', 'Africa/Nairobi', 'Africa/Ndjamena', 'Africa/Sao_Tome', 'Africa/Tripoli', 'Africa/Tunis', 'Africa/Windhoek', 'America/Adak', 'America/Anchorage', 'America/Araguaina', 'America/Argentina/Buenos_Aires', 'America/Argentina/Catamarca', 'America/Argentina/Cordoba', 'America/Argentina/Jujuy', 'America/Argentina/La_Rioja', 'America/Argentina/Mendoza', 'America/Argentina/Rio_Gallegos', 'America/Argentina/Salta', 'America/Argentina/San_Juan', 'America/Argentina/San_Luis', 'America/Argentina/Tucuman', 'America/Argentina/Ushuaia', 'America/Asuncion', 'America/Bahia', 'America/Bahia_Banderas', 'America/Barbados', 'America/Belem', 'America/Belize', 'America/Boa_Vista', 'America/Bogota', 'America/Boise', 'America/Cambridge_Bay', 'America/Campo_Grande', 'America/Cancun', 'America/Caracas', 'America/Cayenne', 'America/Chicago', 'America/Chihuahua', 'America/Ciudad_Juarez', 'America/Costa_Rica', 'America/Cuiaba', 'America/Danmarkshavn', 'America/Dawson', 'America/Dawson_Creek', 'America/Denver', 'America/Detroit', 'America/Edmonton', 'America/Eirunepe', 'America/El_Salvador', 'America/Fort_Nelson', 'America/Fortaleza', 'America/Glace_Bay', 'America/Goose_Bay', 'America/Grand_Turk', 'America/Guatemala', 'America/Guayaquil', 'America/Guyana', 'America/Halifax', 'America/Havana', 'America/Hermosillo', 'America/Indiana/Indianapolis', 'America/Indiana/Knox', 'America/Indiana/Marengo', 'America/Indiana/Petersburg', 'America/Indiana/Tell_City', 'America/Indiana/Vevay', 'America/Indiana/Vincennes', 'America/Indiana/Winamac', 'America/Inuvik', 'America/Iqaluit', 'America/Jamaica', 'America/Juneau', 'America/Kentucky/Louisville', 'America/Kentucky/Monticello', 'America/La_Paz', 'America/Lima', 'America/Los_Angeles', 'America/Maceio', 'America/Managua', 'America/Manaus', 'America/Martinique', 'America/Matamoros', 'America/Mazatlan', 'America/Menominee', 'America/Merida', 'America/Metlakatla', 'America/Mexico_City', 'America/Miquelon', 'America/Moncton', 'America/Monterrey', 'America/Montevideo', 'America/New_York', 'America/Nome', 'America/Noronha', 'America/North_Dakota/Beulah', 'America/North_Dakota/Center', 'America/North_Dakota/New_Salem', 'America/Nuuk', 'America/Ojinaga', 'America/Panama', 'America/Paramaribo', 'America/Phoenix', 'America/Port-au-Prince', 'America/Porto_Velho', 'America/Puerto_Rico', 'America/Punta_Arenas', 'America/Rankin_Inlet', 'America/Recife', 'America/Regina', 'America/Resolute', 'America/Rio_Branco', 'America/Santarem', 'America/Santiago', 'America/Santo_Domingo', 'America/Sao_Paulo', 'America/Scoresbysund', 'America/Sitka', 'America/St_Johns', 'America/Swift_Current', 'America/Tegucigalpa', 'America/Thule', 'America/Tijuana', 'America/Toronto', 'America/Vancouver', 'America/Whitehorse', 'America/Winnipeg', 'America/Yakutat', 'Antarctica/Casey', 'Antarctica/Davis', 'Antarctica/Macquarie', 'Antarctica/Mawson', 'Antarctica/Palmer', 'Antarctica/Rothera', 'Antarctica/Troll', 'Asia/Almaty', 'Asia/Amman', 'Asia/Anadyr', 'Asia/Aqtau', 'Asia/Aqtobe', 'Asia/Ashgabat', 'Asia/Atyrau', 'Asia/Baghdad', 'Asia/Baku', 'Asia/Bangkok', 'Asia/Barnaul', 'Asia/Beirut', 'Asia/Bishkek', 'Asia/Chita', 'Asia/Choibalsan', 'Asia/Colombo', 'Asia/Damascus', 'Asia/Dhaka', 'Asia/Dili', 'Asia/Dubai', 'Asia/Dushanbe', 'Asia/Famagusta', 'Asia/Gaza', 'Asia/Hebron', 'Asia/Ho_Chi_Minh', 'Asia/Hong_Kong', 'Asia/Hovd', 'Asia/Irkutsk', 'Asia/Jakarta', 'Asia/Jayapura', 'Asia/Jerusalem', 'Asia/Kabul', 'Asia/Kamchatka', 'Asia/Karachi', 'Asia/Kathmandu', 'Asia/Khandyga', 'Asia/Kolkata', 'Asia/Krasnoyarsk', 'Asia/Kuching', 'Asia/Macau', 'Asia/Magadan', 'Asia/Makassar', 'Asia/Manila', 'Asia/Nicosia', 'Asia/Novokuznetsk', 'Asia/Novosibirsk', 'Asia/Omsk', 'Asia/Oral', 'Asia/Pontianak', 'Asia/Pyongyang', 'Asia/Qatar', 'Asia/Qostanay', 'Asia/Qyzylorda', 'Asia/Riyadh', 'Asia/Sakhalin', 'Asia/Samarkand', 'Asia/Seoul', 'Asia/Shanghai', 'Asia/Singapore', 'Asia/Srednekolymsk', 'Asia/Taipei', 'Asia/Tashkent', 'Asia/Tbilisi', 'Asia/Tehran', 'Asia/Thimphu', 'Asia/Tokyo', 'Asia/Tomsk', 'Asia/Ulaanbaatar', 'Asia/Urumqi', 'Asia/Ust-Nera', 'Asia/Vladivostok', 'Asia/Yakutsk', 'Asia/Yangon', 'Asia/Yekaterinburg', 'Asia/Yerevan', 'Atlantic/Azores', 'Atlantic/Bermuda', 'Atlantic/Canary', 'Atlantic/Cape_Verde', 'Atlantic/Faroe', 'Atlantic/Madeira', 'Atlantic/South_Georgia', 'Atlantic/Stanley', 'Australia/Adelaide', 'Australia/Brisbane', 'Australia/Broken_Hill', 'Australia/Darwin', 'Australia/Eucla', 'Australia/Hobart', 'Australia/Lindeman', 'Australia/Lord_Howe', 'Australia/Melbourne', 'Australia/Perth', 'Australia/Sydney', 'CET', 'CST6CDT', 'EET', 'EST', 'EST5EDT', 'Etc/GMT', 'Etc/GMT+1', 'Etc/GMT+10', 'Etc/GMT+11', 'Etc/GMT+12', 'Etc/GMT+2', 'Etc/GMT+3', 'Etc/GMT+4', 'Etc/GMT+5', 'Etc/GMT+6', 'Etc/GMT+7', 'Etc/GMT+8', 'Etc/GMT+9', 'Etc/GMT-1', 'Etc/GMT-10', 'Etc/GMT-11', 'Etc/GMT-12', 'Etc/GMT-13', 'Etc/GMT-14', 'Etc/GMT-2', 'Etc/GMT-3', 'Etc/GMT-4', 'Etc/GMT-5', 'Etc/GMT-6', 'Etc/GMT-7', 'Etc/GMT-8', 'Etc/GMT-9', 'Etc/UTC', 'Europe/Andorra', 'Europe/Astrakhan', 'Europe/Athens', 'Europe/Belgrade', 'Europe/Berlin', 'Europe/Brussels', 'Europe/Bucharest', 'Europe/Budapest', 'Europe/Chisinau', 'Europe/Dublin', 'Europe/Gibraltar', 'Europe/Helsinki', 'Europe/Istanbul', 'Europe/Kaliningrad', 'Europe/Kirov', 'Europe/Kyiv', 'Europe/Lisbon', 'Europe/London', 'Europe/Madrid', 'Europe/Malta', 'Europe/Minsk', 'Europe/Moscow', 'Europe/Paris', 'Europe/Prague', 'Europe/Riga', 'Europe/Rome', 'Europe/Samara', 'Europe/Saratov', 'Europe/Simferopol', 'Europe/Sofia', 'Europe/Tallinn', 'Europe/Tirane', 'Europe/Ulyanovsk', 'Europe/Vienna', 'Europe/Vilnius', 'Europe/Volgograd', 'Europe/Warsaw', 'Europe/Zurich', 'HST', 'Indian/Chagos', 'Indian/Maldives', 'Indian/Mauritius', 'MET', 'MST', 'MST7MDT', 'PST8PDT', 'Pacific/Apia', 'Pacific/Auckland', 'Pacific/Bougainville', 'Pacific/Chatham', 'Pacific/Easter', 'Pacific/Efate', 'Pacific/Fakaofo', 'Pacific/Fiji', 'Pacific/Galapagos', 'Pacific/Gambier', 'Pacific/Guadalcanal', 'Pacific/Guam', 'Pacific/Honolulu', 'Pacific/Kanton', 'Pacific/Kiritimati', 'Pacific/Kosrae', 'Pacific/Kwajalein', 'Pacific/Marquesas', 'Pacific/Nauru', 'Pacific/Niue', 'Pacific/Norfolk', 'Pacific/Noumea', 'Pacific/Pago_Pago', 'Pacific/Palau', 'Pacific/Pitcairn', 'Pacific/Port_Moresby', 'Pacific/Rarotonga', 'Pacific/Tahiti', 'Pacific/Tarawa', 'Pacific/Tongatapu', 'WET']
+    time_zone_var = StringVar()
+    time_zone_var.set("Etc/GMT")
+    time_zone_combo_box = ttk.Combobox(mainframe, textvariable=time_zone_var,
+                                            values=list_of_time_zones, state="readonly")
+    time_zone_combo_box.grid(row=3, column=1, columnspan=1)
+    time_zone_combo_box.bind('<<ComboboxSelected>>', lambda e: time_zone_var.set(
+        time_zone_var.get()))
+#    time_zone_combo_box.bind('<<ComboboxSelected>>', lambda e: time_zone_combo_box.selection_clear())
     
     # Set time from defaults
 
